60c05354845f09f5f92253540b09161ff85cb84a
m33i <shionish@gmail.com>
2025-04-17 14:59:51 +0200

    cv2 orb create and detecting keypoints

--- slam.py
+++ slam.py
@@ -1,9 +1,11 @@
 import cv2
+import numpy as np
+from orb_detector import OrbDetector
 
 W = 1920 // 2
 H = 1080 // 2
 
-def process_frame():
+def process_video():
     # open video
     cap = cv2.VideoCapture("video_examples/test_city_bus.mp4")
    
@@ -11,6 +13,9 @@ def process_frame():
     if not cap.isOpened():
         print("err: Could not open video")
         return
+   
+    # Initialize OrbDetector
+    orb_detector = OrbDetector()
    
     while cap.isOpened():
         # read frame, if no frame, end
@@ -20,7 +25,11 @@ def process_frame():
             break
        
         frame = cv2.resize(frame, (W, H))
-        cv2.imshow('frame', frame)
+       
+        # Process the frame with the OrbDetector
+        frame_with_orbs = orb_detector.process_frame(frame)
+       
+        cv2.imshow('frame', frame_with_orbs)
        
         # press Q on keyboard to exit
         if cv2.waitKey(25) & 0xFF == ord('q'):
@@ -31,4 +40,4 @@ def process_frame():
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    process_frame()
+    process_video()
